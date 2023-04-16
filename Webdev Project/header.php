c5c4ee39f63a2ca0d0433aee90f378a923dde404
Tess <89669664+tess-k@users.noreply.github.com>
2023-04-16 13:06:42 -0300

    Update header.php

--- Webdev Project/header.php
+++ Webdev Project/header.php
@@ -1,18 +1,6 @@
 
 <?php
         
-    $dbPassword = "webdevcourse1";
-    $dbUserName = "webdevcourse";
-    $dbServer = "localhost";
-    $dbName = "webdevcourse";
-        
-    $connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
-        
-    if($connection->connect_errno)
-        {
-            exit("Database connection failed. Reason: ".$connection->connect_error);
-        }
-        
     session_start();
         
         
