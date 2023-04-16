12e43dfeca381818305cda6913aafbe765d05fbb
Tess <89669664+tess-k@users.noreply.github.com>
2023-04-16 12:17:52 -0300

    Add files via upload

---
+++ Webdev Project/lost-password.php
@@ -0,0 +1,84 @@
+<?php
+    require "header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!--All the normal setup for a site-->
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halifax Canoe and Kayak | Home</title>
+
+</head>
+
+<!--Into the main body of the text etc.-->
+
+<body>
+
+<!--Navigation bar-->
+
+    <nav class="navbar">
+        <div class="header-logo">
+            <img src="imgs/paddle-white.png" class="logo" width="50px" height="50px" alt="Halifax Canoe and Kayak Logo">
+            <h3>Halifax Canoe & Kayak</h3>
+        </div>
+
+
+        <div>
+            <img src="imgs/hamburger.png" class="menu-button" id="menubtn">
+        </div>
+
+
+        <div class="menu hidden" id="dropmenu">
+            
+              <p><a href="index.php">Home</a>
+              <p><a href="booking.html">Book an Adventure</a></p>
+              <p><a href="admin.php">Admin Login</a></p>
+            
+        </div> 
+
+
+    </nav>
+
+<?php 
+    $emailType = ($_POST["lostPassword"]);
+
+{ ?>
+ <div class="container">
+  <form method="post" action="send-email.php">
+
+ 
+    <input type="radio" id="lostPassword" value="emailType">
+    <label for="lostPassword">I Lost My Password</label>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
+
+<?php
+};
+?>
+
+
+
+ <?php
+    include "footer.php";
+    ?>
+    
+    <span id="logout" position="top right"><a href="logout.php">Log out</a></span>
+    
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="js.js" type="text/javascript"></script>
+
+</body>
+</html>
+    
+    
+    
