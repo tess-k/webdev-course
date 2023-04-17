9b37828673a23177f4609acf4124d7a025739b1e
Tess <89669664+tess-k@users.noreply.github.com>
2023-04-16 22:29:49 -0300

    Update header.php

--- Webdev Project/header.php
+++ Webdev Project/header.php
@@ -1,19 +1,6 @@
 <?php
-session_start();
-
-if (isset($_POST['title']) && isset($_POST['role'])) {
-    if (!empty($_POST['title']) && !empty($_POST['role'])) {
-        $title = $_POST['title'];
-        $role = $_POST['role'];
-    }
-    
-if ($title != "" && $role != "") {
-
-        session_start();
-        $_SESSION["loggedin"] = true;
-        $_SESSION["fname"] = $fname;
-    } else {
-        echo "Please login on the admin page.";
-    }
-}
-
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+?>
