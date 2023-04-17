9fc54345d885fcc6ee44c6385abe2543ecda272b
Tess <89669664+tess-k@users.noreply.github.com>
2023-04-16 22:31:14 -0300

    Update logout.php

--- Webdev Project/logout.php
+++ Webdev Project/logout.php
@@ -47,18 +47,6 @@
 
     <?php
 
-    $_SESSION = [];
-    $ses_params = session_get_cookie_params();
-
-    $options = array(
-    'lifetime' => time()-60,
-    'path'     => $ses_params['path'],
-    'domain'   => $ses_params['domain'],
-    'secure'   => $ses_params['secure'],
-    'httponly' => $ses_params['httponly'],
-    'samesite' => $ses_params['samesite']);
-
-    setcookie(session_name(), '', $options);
     session_destroy();
     
     ?>
